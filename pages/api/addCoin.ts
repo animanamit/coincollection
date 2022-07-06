d8736e3d45b8cebdedc416347f7fdd5e1a5b3238
Animan Amit <animanamit@wustl.edu>
2022-07-06 23:08:27 +0800

    ...

--- pages/api/addCoin.ts
+++ pages/api/addCoin.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-export default async function handler(req, res) {
+export default async function handler(req: any, res: any) {
   let dataObj = req.body;
 
   try {
