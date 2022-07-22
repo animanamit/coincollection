a7da8ddd8f41e2ae1d26ed446f5a93105c87e804
Animan Amit <animanamit@wustl.edu>
2022-07-22 21:28:10 +0800

    ....can't get overlap to work correctly, leaving it for now

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className=" bg-white top-0 z-20 backdrop-filter backdrop-blur-lg bg-opacity-30 px-12 border-b border-gray-200">
+    <div className="sticky bg-white top-0 z-20 backdrop-filter backdrop-blur-lg bg-opacity-30 px-12 border-b border-gray-200">
       <div className="flex justify-end mx-auto items-center py-4">
         {/* <div>
           <h1 className="text-gray-900 text-xl">Amit Srivastava</h1>
